90cb6793bb01b13f1fdc3a1770fa7a2a2f01e585
yink12138 <1097179128@qq.com>
2021-11-18 18:56:31 +0800

    zhangjy

--- Assignment.srcs/sim_1/new/led_sim.sv
+++ Assignment.srcs/sim_1/new/led_sim.sv
@@ -8,16 +8,21 @@ module led_sim();
     wire [7:0] display_onoff;
     wire [0:6] display0;
     wire [0:6] display1;
+    reg mode1;
+    wire input_finish;
 
     clock clock(clk);
-    sec_divider sec_divider(clk, clk_sec);
-    entry entry(clk, user_input, display_onoff, display0, display1);
+    //sec_divider sec_divider(clk, clk_sec);
+    entry entry(clk, user_input, display_onoff, display0, display1, mode1,input_finish);
 
     initial begin
+        mode1=0;
         user_input = 0;
+        #5;
+        mode1=1;
     end
 
-    always @(posedge clk_sec) begin
+    always @(posedge clk) begin
         if (user_input == 0) user_input = 8'b00000001;
         user_input <<= 1;
     end
